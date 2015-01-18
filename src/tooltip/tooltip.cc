jmc-88/tint3

commit ae47f3821895d0bd8e241a4817a011349ce2be35

Rename position fields to be less ambiguous.
The fields Panel::pos_x_ and Panel::pos_y_ are not the same as
Area::posx_ and Area::posy_, but they could've easily been confused.

src/tooltip/tooltip.cc
@@ -131,8 +131,8 @@ void InitTooltip() {
 
 void TooltipTriggerShow(Area* area, Panel* p, XEvent* e) {
     // Position the tooltip in the center of the area
-    x = area->posx_ + area->width_ / 2 + e->xmotion.x_root - e->xmotion.x;
-    y = area->posy_ + area->height_ / 2 + e->xmotion.y_root - e->xmotion.y;
+    x = area->panel_x_ + area->width_ / 2 + e->xmotion.x_root - e->xmotion.x;
+    y = area->panel_y_ + area->height_ / 2 + e->xmotion.y_root - e->xmotion.y;
 
     if (!panel_horizontal) {
         y -= height / 2;
@@ -191,13 +191,13 @@ void TooltipUpdateGeometry() {
     Panel* panel = g_tooltip.panel;
 
     if (panel_horizontal && panel_position & BOTTOM) {
-        y = panel->posy_ - height;
+        y = panel->panel_y_ - height;
     } else if (panel_horizontal && panel_position & TOP) {
-        y = panel->posy_ + panel->height_;
+        y = panel->panel_y_ + panel->height_;
     } else if (panel_position & LEFT) {
-        x = panel->posx_ + panel->width_;
+        x = panel->panel_x_ + panel->width_;
     } else {
-        x = panel->posx_ - width;
+        x = panel->panel_x_ - width;
     }
 
     g_object_unref(layout);

src/launcher/launcher.cc
@@ -303,8 +303,8 @@ LauncherIcon::~LauncherIcon() {
 // Here we override the default layout of the icons; normally Area layouts its children
 // in a stack; we need to layout them in a kind of table
 void LauncherIcon::OnChangeLayout() {
-    posy_ = (parent_->posy_ + y_);
-    posx_ = (parent_->posx_ + x_);
+    panel_y_ = (parent_->panel_y_ + y_);
+    panel_x_ = (parent_->panel_x_ + x_);
 }
 
 std::string LauncherIcon::GetTooltipText() {

src/panel.cc
@@ -110,14 +110,14 @@ void UpdateStrut(Panel* p) {
 
         if (panel_position & TOP) {
             struts[2] = height + monitor.y;
-            struts[8] = p->pos_x_;
+            struts[8] = p->root_x_;
             // p->width - 1 allowed full screen on monitor 2
-            struts[9] = p->pos_x_ + p->width_ - 1;
+            struts[9] = p->root_x_ + p->width_ - 1;
         } else {
             struts[3] = height + screen_height - monitor.y - monitor.height;
-            struts[10] = p->pos_x_;
+            struts[10] = p->root_x_;
             // p->width - 1 allowed full screen on monitor 2
-            struts[11] = p->pos_x_ + p->width_ - 1;
+            struts[11] = p->root_x_ + p->width_ - 1;
         }
     } else {
         int width = p->width_ + p->margin_x_;
@@ -129,14 +129,14 @@ void UpdateStrut(Panel* p) {
 
         if (panel_position & LEFT) {
             struts[0] = width + monitor.x;
-            struts[4] = p->pos_y_;
+            struts[4] = p->root_y_;
             // p->width - 1 allowed full screen on monitor 2
-            struts[5] = p->pos_y_ + p->height_ - 1;
+            struts[5] = p->root_y_ + p->height_ - 1;
         } else {
             struts[1] = width + screen_width - monitor.x - monitor.width;
-            struts[6] = p->pos_y_;
+            struts[6] = p->root_y_;
             // p->width - 1 allowed full screen on monitor 2
-            struts[7] = p->pos_y_ + p->height_ - 1;
+            struts[7] = p->root_y_ + p->height_ - 1;
         }
     }
 
@@ -318,7 +318,8 @@ void InitPanel() {
             attr.border_pixel = 0;
 
             unsigned long mask = CWEventMask | CWColormap | CWBackPixel | CWBorderPixel;
-            p->main_win_ = XCreateWindow(server.dsp, server.root_win, p->pos_x_, p->pos_y_,
+            p->main_win_ = XCreateWindow(server.dsp, server.root_win, p->root_x_,
+                                         p->root_y_,
                                          p->width_, p->height_, 0, server.depth, InputOutput, server.visual,
                                          mask, &attr);
         }
@@ -418,33 +419,33 @@ void Panel::InitSizeAndPosition() {
 
     // panel position determined here
     if (panel_position & LEFT) {
-        pos_x_ = server.monitor[monitor_].x + margin_x_;
+        root_x_ = server.monitor[monitor_].x + margin_x_;
     } else {
         if (panel_position & RIGHT) {
-            pos_x_ = server.monitor[monitor_].x + server.monitor[monitor_].width - width_ -
-                     margin_x_;
+            root_x_ = server.monitor[monitor_].x + server.monitor[monitor_].width - width_ -
+                      margin_x_;
         } else {
             if (panel_horizontal) {
-                pos_x_ = server.monitor[monitor_].x + ((server.monitor[monitor_].width -
+                root_x_ = server.monitor[monitor_].x + ((server.monitor[monitor_].width -
                                                         width_) /
-                                                       2);
+                                                        2);
             } else {
-                pos_x_ = server.monitor[monitor_].x + margin_x_;
+                root_x_ = server.monitor[monitor_].x + margin_x_;
             }
         }
     }
 
     if (panel_position & TOP) {
-        pos_y_ = server.monitor[monitor_].y + margin_y_;
+        root_y_ = server.monitor[monitor_].y + margin_y_;
     } else {
         if (panel_position & BOTTOM) {
-            pos_y_ = server.monitor[monitor_].y + server.monitor[monitor_].height - height_
-                     -
-                     margin_y_;
+            root_y_ = server.monitor[monitor_].y + server.monitor[monitor_].height - height_
+                      -
+                      margin_y_;
         } else {
-            pos_y_ = server.monitor[monitor_].y + ((server.monitor[monitor_].height -
+            root_y_ = server.monitor[monitor_].y + ((server.monitor[monitor_].height -
                                                     height_) /
-                                                   2);
+                                                    2);
         }
     }
 
@@ -726,8 +727,8 @@ Taskbar* Panel::ClickTaskbar(int x, int y) {
         for (int i = 0; i < nb_desktop_ ; i++) {
             Taskbar* tskbar = &taskbar_[i];
 
-            if (tskbar->on_screen_ && x >= tskbar->posx_
-                && x <= (tskbar->posx_ + tskbar->width_)) {
+            if (tskbar->on_screen_ && x >= tskbar->panel_x_
+                && x <= (tskbar->panel_x_ + tskbar->width_)) {
                 return tskbar;
             }
         }
@@ -735,8 +736,8 @@ Taskbar* Panel::ClickTaskbar(int x, int y) {
         for (int i = 0; i < nb_desktop_ ; i++) {
             Taskbar* tskbar = &taskbar_[i];
 
-            if (tskbar->on_screen_ && y >= tskbar->posy_
-                && y <= (tskbar->posy_ + tskbar->height_)) {
+            if (tskbar->on_screen_ && y >= tskbar->panel_y_
+                && y <= (tskbar->panel_y_ + tskbar->height_)) {
                 return tskbar;
             }
         }
@@ -760,13 +761,13 @@ Task* Panel::ClickTask(int x, int y) {
             auto tsk = reinterpret_cast<Task*>(*it);
 
             if (panel_horizontal) {
-                if (tsk->on_screen_ && x >= tsk->posx_
-                    && x <= (tsk->posx_ + tsk->width_)) {
+                if (tsk->on_screen_ && x >= tsk->panel_x_
+                    && x <= (tsk->panel_x_ + tsk->width_)) {
                     return tsk;
                 }
             } else {
-                if (tsk->on_screen_ && y >= tsk->posy_
-                    && y <= (tsk->posy_ + tsk->height_)) {
+                if (tsk->on_screen_ && y >= tsk->panel_y_
+                    && y <= (tsk->panel_y_ + tsk->height_)) {
                     return tsk;
                 }
             }
@@ -779,13 +780,13 @@ Task* Panel::ClickTask(int x, int y) {
 
 Launcher* Panel::ClickLauncher(int x, int y) {
     if (panel_horizontal) {
-        if (launcher_.on_screen_ && x >= launcher_.posx_ &&
-            x <= (launcher_.posx_ + launcher_.width_)) {
+        if (launcher_.on_screen_ && x >= launcher_.panel_x_ &&
+            x <= (launcher_.panel_x_ + launcher_.width_)) {
             return &launcher_;
         }
     } else {
-        if (launcher_.on_screen_ && y >= launcher_.posy_ &&
-            y <= (launcher_.posy_ + launcher_.height_)) {
+        if (launcher_.on_screen_ && y >= launcher_.panel_y_ &&
+            y <= (launcher_.panel_y_ + launcher_.height_)) {
             return &launcher_;
         }
     }
@@ -799,10 +800,10 @@ LauncherIcon* Panel::ClickLauncherIcon(int x, int y) {
 
     if (launcher != nullptr) {
         for (auto& launcher_icon : launcher->list_icons_) {
-            int base_x = (launcher->posx_ + launcher_icon->x_);
+            int base_x = (launcher->panel_x_ + launcher_icon->x_);
             bool inside_x = (x >= base_x &&
                              x <= (base_x + launcher_icon->icon_size_));
-            int base_y = (launcher->posy_ + launcher_icon->y_);
+            int base_y = (launcher->panel_y_ + launcher_icon->y_);
             bool inside_y = (y >= base_y &&
                              y <= (base_y + launcher_icon->icon_size_));
 
@@ -833,13 +834,13 @@ bool Panel::ClickPadding(int x, int y) {
 
 bool Panel::ClickClock(int x, int y) {
     if (panel_horizontal) {
-        if (clock_.on_screen_ && x >= clock_.posx_
-            && x <= (clock_.posx_ + clock_.width_)) {
+        if (clock_.on_screen_ && x >= clock_.panel_x_
+            && x <= (clock_.panel_x_ + clock_.width_)) {
             return true;
         }
     } else {
-        if (clock_.on_screen_ && y >= clock_.posy_
-            && y <= (clock_.posy_ + clock_.height_)) {
+        if (clock_.on_screen_ && y >= clock_.panel_y_
+            && y <= (clock_.panel_y_ + clock_.height_)) {
             return true;
         }
     }
@@ -858,8 +859,8 @@ Area* Panel::ClickArea(int x, int y) {
         for (auto it = result->children_.begin(); it != result->children_.end(); ++it) {
             Area* a = (*it);
 
-            if (a->on_screen_ && x >= a->posx_ && x <= (a->posx_ + a->width_)
-                && y >= a->posy_ && y <= (a->posy_ + a->height_)) {
+            if (a->on_screen_ && x >= a->panel_x_ && x <= (a->panel_x_ + a->width_)
+                && y >= a->panel_y_ && y <= (a->panel_y_ + a->height_)) {
                 new_result = a;
                 break;
             }
@@ -920,7 +921,7 @@ void AutohideShow(void* p) {
             XResizeWindow(server.dsp, panel->main_win_, panel->width_,
                           panel->height_);
         } else {
-            XMoveResizeWindow(server.dsp, panel->main_win_, panel->pos_x_, panel->pos_y_,
+            XMoveResizeWindow(server.dsp, panel->main_win_, panel->root_x_, panel->root_y_,
                               panel->width_, panel->height_);
         }
     } else {
@@ -928,7 +929,7 @@ void AutohideShow(void* p) {
             XResizeWindow(server.dsp, panel->main_win_, panel->width_,
                           panel->height_);
         } else {
-            XMoveResizeWindow(server.dsp, panel->main_win_, panel->pos_x_, panel->pos_y_,
+            XMoveResizeWindow(server.dsp, panel->main_win_, panel->root_x_, panel->root_y_,
                               panel->width_, panel->height_);
         }
     }
@@ -958,8 +959,8 @@ void AutohideHide(void* p) {
             XResizeWindow(server.dsp, panel->main_win_, panel->hidden_width_,
                           panel->hidden_height_);
         } else {
-            XMoveResizeWindow(server.dsp, panel->main_win_, panel->pos_x_,
-                              panel->pos_y_ + diff,
+            XMoveResizeWindow(server.dsp, panel->main_win_, panel->root_x_,
+                              panel->root_y_ + diff,
                               panel->hidden_width_, panel->hidden_height_);
         }
     } else {
@@ -967,8 +968,8 @@ void AutohideHide(void* p) {
             XResizeWindow(server.dsp, panel->main_win_, panel->hidden_width_,
                           panel->hidden_height_);
         } else {
-            XMoveResizeWindow(server.dsp, panel->main_win_, panel->pos_x_ + diff,
-                              panel->pos_y_,
+            XMoveResizeWindow(server.dsp, panel->main_win_, panel->root_x_ + diff,
+                              panel->root_y_,
                               panel->hidden_width_, panel->hidden_height_);
         }
     }

src/panel.h
@@ -83,7 +83,7 @@ class Panel : public Area {
     Pixmap temp_pmap;
 
     // position relative to root window
-    int pos_x_, pos_y_;
+    int root_x_, root_y_;
     int margin_x_, margin_y_;
     int percent_x, percent_y;
     // location of the panel (monitor number)

src/systray/systraybar.cc
@@ -174,11 +174,11 @@ void Systraybar::OnChangeLayout() {
 
     if (panel_horizontal) {
         posy = start;
-        posx = systray.posx_ + systray.bg_->border.width +
+        posx = systray.panel_x_ + systray.bg_->border.width +
                systray.padding_x_lr_;
     } else {
         posx = start;
-        posy = systray.posy_ + systray.bg_->border.width +
+        posy = systray.panel_y_ + systray.bg_->border.width +
                systray.padding_x_lr_;
     }
 
@@ -664,13 +664,13 @@ void systray_render_icon_now(void* t) {
 
     imlib_image_put_back_data(data);
     XCopyArea(server.dsp, render_background, systray.pix_, server.gc,
-              traywin->x - systray.posx_, traywin->y - systray.posy_, traywin->width,
-              traywin->height, traywin->x - systray.posx_,
-              traywin->y - systray.posy_);
-    RenderImage(systray.pix_, traywin->x - systray.posx_,
-                traywin->y - systray.posy_, traywin->width, traywin->height);
+              traywin->x - systray.panel_x_, traywin->y - systray.panel_y_, traywin->width,
+              traywin->height, traywin->x - systray.panel_x_,
+              traywin->y - systray.panel_y_);
+    RenderImage(systray.pix_, traywin->x - systray.panel_x_,
+                traywin->y - systray.panel_y_, traywin->width, traywin->height);
     XCopyArea(server.dsp, systray.pix_, panel->main_win_, server.gc,
-              traywin->x - systray.posx_, traywin->y - systray.posy_, traywin->width,
+              traywin->x - systray.panel_x_, traywin->y - systray.panel_y_, traywin->width,
               traywin->height, traywin->x, traywin->y);
     imlib_free_image_and_decache();
     XFreePixmap(server.dsp, tmp_pmap);

src/taskbar/task.cc
@@ -500,8 +500,8 @@ void Task::DrawForeground(cairo_t* c) {
 
 void Task::OnChangeLayout() {
     long value[] = {
-        panel_->posx_ + posx_,
-        panel_->posy_ + posy_,
+        panel_->panel_x_ + panel_x_,
+        panel_->panel_y_ + panel_y_,
         width_,
         height_
     };

src/taskbar/taskbar.cc
@@ -205,14 +205,14 @@ void InitTaskbarPanel(Panel* panel) {
     panel->g_taskbar.on_screen_ = 1;
 
     if (panel_horizontal) {
-        panel->g_taskbar.posy_ = (panel->bg_->border.width + panel->padding_y_);
-        panel->g_taskbar.height_ = panel->height_ - (2 * panel->g_taskbar.posy_);
-        panel->g_taskbar.bar_name_.posy_ = panel->g_taskbar.posy_;
+        panel->g_taskbar.panel_y_ = (panel->bg_->border.width + panel->padding_y_);
+        panel->g_taskbar.height_ = panel->height_ - (2 * panel->g_taskbar.panel_y_);
+        panel->g_taskbar.bar_name_.panel_y_ = panel->g_taskbar.panel_y_;
         panel->g_taskbar.bar_name_.height_ = panel->g_taskbar.height_;
     } else {
-        panel->g_taskbar.posx_ = panel->bg_->border.width + panel->padding_y_;
-        panel->g_taskbar.width_ = panel->width_ - (2 * panel->g_taskbar.posx_);
-        panel->g_taskbar.bar_name_.posx_ = panel->g_taskbar.posx_;
+        panel->g_taskbar.panel_x_ = panel->bg_->border.width + panel->padding_y_;
+        panel->g_taskbar.width_ = panel->width_ - (2 * panel->g_taskbar.panel_x_);
+        panel->g_taskbar.bar_name_.panel_x_ = panel->g_taskbar.panel_x_;
         panel->g_taskbar.bar_name_.width_ = panel->g_taskbar.width_;
     }
 
@@ -283,15 +283,15 @@ void InitTaskbarPanel(Panel* panel) {
     }
 
     if (panel_horizontal) {
-        panel->g_task.posy_ = panel->g_taskbar.posy_ +
-                              panel->g_taskbar.background[TASKBAR_NORMAL]->border.width +
-                              panel->g_taskbar.padding_y_;
-        panel->g_task.height_ = panel->height_ - (2 * panel->g_task.posy_);
+        panel->g_task.panel_y_ = panel->g_taskbar.panel_y_ +
+                                 panel->g_taskbar.background[TASKBAR_NORMAL]->border.width +
+                                 panel->g_taskbar.padding_y_;
+        panel->g_task.height_ = panel->height_ - (2 * panel->g_task.panel_y_);
     } else {
-        panel->g_task.posx_ = panel->g_taskbar.posx_ +
-                              panel->g_taskbar.background[TASKBAR_NORMAL]->border.width +
-                              panel->g_taskbar.padding_y_;
-        panel->g_task.width_ = panel->width_ - (2 * panel->g_task.posx_);
+        panel->g_task.panel_x_ = panel->g_taskbar.panel_x_ +
+                                 panel->g_taskbar.background[TASKBAR_NORMAL]->border.width +
+                                 panel->g_taskbar.padding_y_;
+        panel->g_task.width_ = panel->width_ - (2 * panel->g_task.panel_x_);
         panel->g_task.height_ = panel->g_task.maximum_height;
     }
 

src/taskbar/taskbarname.cc
@@ -139,7 +139,7 @@ void Taskbarname::DrawForeground(cairo_t* c) {
                           config_text->color[2], config_text->alpha);
 
     pango_cairo_update_layout(c, layout);
-    cairo_move_to(c, 0, posy_);
+    cairo_move_to(c, 0, panel_y_);
     pango_cairo_show_layout(c, layout);
 
     g_object_unref(layout);
@@ -162,7 +162,7 @@ bool Taskbarname::Resize() {
 
         if (new_size != width_) {
             width_ = new_size;
-            posy_ = (height_ - name_height) / 2;
+            panel_y_ = (height_ - name_height) / 2;
             return true;
         }
     } else {
@@ -170,7 +170,7 @@ bool Taskbarname::Resize() {
 
         if (new_size != height_) {
             height_ = new_size;
-            posy_ = (height_ - name_height) / 2;
+            panel_y_ = (height_ - name_height) / 2;
             return true;
         }
     }

commit d423e47e749f30b226ef71375a79f02a8ca7390a

Use root_x_ and root_y_ for Tooltip coordinates.
This got broken by ae47f3821895d0bd8e241a4817a011349ce2be35. There might
be more to fix from that commit.

src/tooltip/tooltip.cc
@@ -188,13 +188,13 @@ void TooltipUpdateGeometry() {
   Panel* panel = g_tooltip.panel;
 
   if (panel_horizontal && panel_position & kBottom) {
-    y = panel->panel_y_ - height;
+    y = panel->root_y_ - height;
   } else if (panel_horizontal && panel_position & kTop) {
-    y = panel->panel_y_ + panel->height_;
+    y = panel->root_y_ + panel->height_;
   } else if (panel_position & kLeft) {
-    x = panel->panel_x_ + panel->width_;
+    x = panel->root_x_ + panel->width_;
   } else {
-    x = panel->panel_x_ - width;
+    x = panel->root_x_ - width;
   }
 
   cairo_destroy(c);