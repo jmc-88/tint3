e1d894534e7ceeb423cdc1f8ced08e2d612215aa
Daniele Cocca <daniele.cocca@gmail.com>
2015-07-08 00:58:45 +0100

    Add a compile option for disabling ASan for tests.
    Leave it enabled by default, as it's nice to have, but can fail linking on some
    systems (yes, Ubuntu, I'm looking at you), so give a way to easily turn it off
    if really needed.

--- cmake/Modules/TestTarget.cmake
+++ cmake/Modules/TestTarget.cmake
@@ -1,11 +1,17 @@
+option(TINT3_ENABLE_ASAN_FOR_TESTS
+       "Whether to use AddressSanitizer for test binaries"
+       ON)
+
 function(test_target target_name)
   add_executable(${target_name} ${ARGN})
   add_test(NAME ${target_name} COMMAND ${target_name})
 
-  if("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
-    # When using Clang, turn on AddressSanitizer by default for all
-    # the test targets.
-    set_target_properties(${target_name} PROPERTIES
-                          LINK_FLAGS "-fsanitize=address -fno-omit-frame-pointer")
+  if(${TINT3_ENABLE_ASAN_FOR_TESTS})
+    if("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+      # When using Clang, turn on AddressSanitizer by default for all
+      # the test targets.
+      set_target_properties(${target_name} PROPERTIES
+                            LINK_FLAGS "-fsanitize=address -fno-omit-frame-pointer")
+    endif()
   endif()
 endfunction(test_target)
