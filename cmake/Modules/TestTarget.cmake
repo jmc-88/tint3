68701142a46ff5481d4618cf90ae27d9f23b8376
Daniele Cocca <daniele.cocca@gmail.com>
2017-05-31 23:58:42 +0100

    Improve the test_target rule
    Have it receive multivalued arguments and automatically add the needed
    sources, dependencies and link libraries. Rewrite the test targets
    definitions in CMakeLists.txt accordingly.

--- cmake/Modules/TestTarget.cmake
+++ cmake/Modules/TestTarget.cmake
@@ -1,10 +1,22 @@
+include(CMakeParseArguments)
 option(TINT3_ENABLE_ASAN_FOR_TESTS
        "Whether to use AddressSanitizer for test binaries"
        ON)
 
 function(test_target target_name)
-  add_executable(${target_name} ${ARGN})
+  set(multiValueArgs SOURCES DEPENDS LINK_LIBRARIES)
+  cmake_parse_arguments(TEST_TARGET "" "" "${multiValueArgs}" ${ARGN})
+
+  add_executable(${target_name} ${TEST_TARGET_SOURCES})
   add_test(NAME ${target_name} COMMAND ${target_name})
+
+  if(TEST_TARGET_LINK_LIBRARIES)
+    target_link_libraries(${target_name} ${TEST_TARGET_LINK_LIBRARIES})
+  endif()
+
+  if(TEST_TARGET_DEPENDS)
+    add_dependencies(${target_name} ${TEST_TARGET_DEPENDS})
+  endif()
 
   if(${TINT3_ENABLE_ASAN_FOR_TESTS})
     if("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
