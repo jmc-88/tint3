fc908ec097a98b012ce71c0a3d5873c9bf2aa997
Daniele Cocca <daniele.cocca@gmail.com>
2017-06-09 00:21:07 +0100

    Fix panel_test
    Make Clang and LeakSanitizer happy.

--- src/panel_test.cc
+++ src/panel_test.cc
@@ -3,17 +3,22 @@
 #include "panel.hh"
 #include "server.hh"
 
+Monitor TestMonitor() {
+  Monitor m;
+  m.x = 0;
+  m.y = 0;
+  m.width = 1024;
+  m.height = 768;
+  m.names = {"test"};
+  return m;
+}
+
 TEST_CASE("InitSizeAndPosition") {
-  server.monitor.push_back(Monitor{
-    x : 0,
-    y : 0,
-    width : 1024,
-    height : 768,
-    names : {"Test"},
-  });
+  server.monitor.push_back(TestMonitor());
 
   SECTION("size != 0") {
     Panel p;
+    p.monitor_ = 0;
     p.width_ = p.height_ = 0;
 
     p.InitSizeAndPosition();
