30147d108e401042da202aa9bfe4ff0f96c990bc
Daniele Cocca <daniele.cocca@gmail.com>
2017-08-27 00:11:56 +0100

    panel.cc: test SetItemsOrder for executors

--- src/panel_test.cc
+++ src/panel_test.cc
@@ -26,3 +26,48 @@ TEST_CASE("InitSizeAndPosition") {
     REQUIRE(p.height_ != 0);
   }
 }
+
+TEST_CASE("SetItemsOrder_Executors") {
+  // Add three (empty) executors for testing.
+  executors.clear();
+  executors.push_back(Executor{});
+  executors.push_back(Executor{});
+  executors.push_back(Executor{});
+
+  SECTION("add only one") {
+    panel_items_order = "E";
+    Panel p;
+    p.SetItemsOrder();
+    REQUIRE(p.children_.size() == 1);
+    REQUIRE(p.children_.at(0) == &executors.at(0));
+  }
+
+  SECTION("add only two") {
+    panel_items_order = "EE";
+    Panel p;
+    p.SetItemsOrder();
+    REQUIRE(p.children_.size() == 2);
+    REQUIRE(p.children_.at(0) == &executors.at(0));
+    REQUIRE(p.children_.at(1) == &executors.at(1));
+  }
+
+  SECTION("add all three") {
+    panel_items_order = "EEE";
+    Panel p;
+    p.SetItemsOrder();
+    REQUIRE(p.children_.size() == 3);
+    REQUIRE(p.children_.at(0) == &executors.at(0));
+    REQUIRE(p.children_.at(1) == &executors.at(1));
+    REQUIRE(p.children_.at(2) == &executors.at(2));
+  }
+
+  SECTION("add too many") {
+    panel_items_order = "EEEE";
+    Panel p;
+    p.SetItemsOrder();
+    REQUIRE(p.children_.size() == 3);
+    REQUIRE(p.children_.at(0) == &executors.at(0));
+    REQUIRE(p.children_.at(1) == &executors.at(1));
+    REQUIRE(p.children_.at(2) == &executors.at(2));
+  }
+}
